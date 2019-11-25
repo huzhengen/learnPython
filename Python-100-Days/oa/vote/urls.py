93a1de4cfff383ec77271d0b3d243738b89aae35
hukai <huzhengen@163.com>
2019-11-25 16:11:43 +0800

    ajax点赞

--- Python-100-Days/oa/vote/urls.py
+++ Python-100-Days/oa/vote/urls.py
@@ -6,4 +6,6 @@ from vote import views
 urlpatterns = [
     path('', views.show_subjects),
     path('teachers/', views.show_teachers),
+    path('praise/', views.praise_or_criticize),
+    path('criticize/', views.praise_or_criticize),
 ]
--- Python-100-Days/oa/vote/views.py
+++ Python-100-Days/oa/vote/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from vote.models import Subject
-
+from vote.models import Subject, Teacher
+from django.http import JsonResponse
 
 def show_subjects(request):
     """查看所有学科"""
@@ -16,3 +16,18 @@ def show_teachers(request):
         return render(request, 'teachers.html', {'subject': subject, 'teachers': teachers})
     except (KeyError, ValueError, Subject.DoesNotExist):
         return redirect('/')
+
+def praise_or_criticize(request):
+    """好评"""
+    try:
+        tno = int(request.GET['tno'])
+        teacher = Teacher.objects.get(no=tno)
+        if request.path.startswith('/praise'):
+            teacher.good_count += 1
+        else:
+            teacher.bad_count += 1
+        teacher.save()
+        data = {'code': 200, 'hint': '操作成功'}
+    except (KeyError, ValueError, Teacher.DoseNotExist):
+        data = {'code': 404, 'hint': '操作失败'}
+    return JsonResponse(data)
